7ac4792682fda1c013dcf9ae888d0dbbfc7b6dc1
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-11 21:10:30 -0800

    add hero image with text

--- components/home/hero.js
+++ components/home/hero.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default function Hero({ content }) {
     return (
-        <div>
-            {content.heading} 
-            <img src={content.image[0].url} />
-        </div>
+        <header> 
+            <figure>
+                <img src={content.image[0].url} className="object-scale-down" />
+                <figcaption className="absolute text-xl -mt-60 text-main-blue pl-4">
+                    <div className="bg-orange w-8/12">
+                        <h1>{content.heading}</h1>
+                    </div>
+                </figcaption>
+            </figure>
+        </header>
     )
 };
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'orange': '#fdb913'
+    },
+    extend: {},
+    textColor: {
+      'main-blue': '#026cb6', 
+      'orange': '#fdb913'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
