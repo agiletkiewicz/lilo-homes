771d75164e21242fb2f0bba857057f12bdc630e3
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-11 17:15:04 -0800

    fetch data from airtable

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,31 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Airtable from 'airtable';
+import axios from 'axios';
 
-export default function Home() {
+const airtableBaseUrl = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}`;
+const airtableConfig = {
+  headers: {
+    Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+  } 
+};
+
+export default function Home({ records }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      {console.log(records)}
     </div>
   )
+}
+
+export async function getStaticProps() {
+
+  const response = await axios.get(`${airtableBaseUrl}/home`, airtableConfig );
+
+  const { data } = await response;
+  const { records } = await data;
+
+  return {
+    props: { records }
+  }
 }
