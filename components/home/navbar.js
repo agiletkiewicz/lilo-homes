f84e6a3e52c337453f99fc866b42274c2dae24e1
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-13 20:12:36 -0800

    render logo and menu icons

--- components/home/navbar.js
+++ components/home/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function NavBar({ content }) {
     return (
         <nav className="flex justify-between"> 
             <div><Image src='/logo.png' width="75" height="54"/></div>
-            <div><Image src='/menu.png' width="25" height="25"/></div>
+            <div className="pr-3 pt-3"><Image src='/menu.png' width="40" height="30"/></div>
         </nav>
     )
 };
+
