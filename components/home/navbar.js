6b0da6440dd3a2451d66ce2a531a0407ba09ef2e
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-13 17:59:19 -0800

    mobile menu

--- components/home/navbar.js
+++ components/home/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar({ content }) {
     return (
         <nav className="flex justify-between"> 
             <div><Image src='/logo.png' width="75" height="54"/></div>
-            <div>#2</div>
+            <div><Image src='/menu.png' width="25" height="25"/></div>
         </nav>
     )
 };
