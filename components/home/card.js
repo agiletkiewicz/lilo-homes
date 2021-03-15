a333cd0d5b9ea3085d0027d8e67fad427869fa47
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-14 20:54:27 -0700

    add filter and names to house cards

--- components/home/card.js
+++ components/home/card.js
@@ -3,6 +3,12 @@ import Image from 'next/image';
 
 export default function Card({ content }) {
     return (
-        <img src={content.image[0].url} className="pt-6 pb-3" />
+        <div className="relative mt-6 mb-3">
+            <img src={content.image[0].url} className="" />
+            <div className="absolute top-0 bottom-0 left-0 right-0 bg-grey opacity-60"></div>
+                <div className="absolute top-0 bottom-0 left-0 right-0 bg-neutral m-auto h-6 w-28 text-center">
+                     <p className="">{content.heading}</p>
+                 </div>
+        </div>
     )
 };
