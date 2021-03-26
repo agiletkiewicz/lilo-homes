6806380fccaa0389468d930cab40140bd8a6998a
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-26 14:52:05 -0700

    add bungalows page

---
+++ pages/bungalow.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Airtable from 'airtable';
+import axios from 'axios';
+import Hero from '../components/home/hero';
+import NavBar from '../components/home/navbar';
+import Heading from '../components/home/heading';
+import Card from '../components/home/card';
+import About from '../components/home/about';
+
+
+
+
+const airtableBaseUrl = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}`;
+const airtableConfig = {
+  headers: {
+    Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+  } 
+};
+
+export default function Home({ about, hero, home, house, bungalow }) {
+  return (
+      <>
+      <NavBar />
+
+      <About content={about} />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+
+  const response = await axios.get(`${airtableBaseUrl}/home`, airtableConfig );
+
+  const { data } = await response;
+  const { records } = await data;
+
+  let content = {};
+  records.forEach((record) => {
+    content[record.fields.key] = record.fields
+  })
+
+  return {
+    props: {...content},
+    revalidate: 60
+  }
+  
+}
