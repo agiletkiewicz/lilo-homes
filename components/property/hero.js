d3f1e48264c181de16e44b512de6c6d4e0f778b6
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-29 22:36:53 -0700

    style text on bungalow page

--- components/property/hero.js
+++ components/property/hero.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Hero({ content }) {
     return (
         <header>
-            <h1>{content.heading}</h1>
-            <img src={content.image[0].url} />
-            <p>{content.text}</p>
+            <h1 className="text-3xl lg:text-4xl text-main-blue p-2 font-heading">{content.heading}</h1>
+            <img src={content.image[0].url} className="object-cover h-100" />
+            <p className="p-6 font-body lg:text-xl">{content.text}</p>
         </header>
     )
 };
