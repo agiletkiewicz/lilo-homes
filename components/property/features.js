1a1b33127d5ed8bb6a0b26119bca9904df340c40
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-30 21:49:53 -0700

    add icons to features section

--- components/property/features.js
+++ components/property/features.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Icons from './icons'
 
 export default function Features({ content }) {
     return (
         <section className="bg-droplet-dark lg:px-40 lg:py-10 lg:text-center"> 
             <h1 className="font-heading text-white text-3xl lg:text-4xl pb-5">{content.heading}</h1>
+            <Icons />
             <p className="font-body text-lg text-white">{content.text}</p>
         </section>
     )
---
+++ components/property/icons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Icons() {
+    return (
+        <div className="h-24 flex justify-center m-5">
+            <img src='/living.png' className="h-24" />
+            <img src='/washing.png' className="h-24" />
+            <img src='/smart.png' className="h-24" />
+            <img src='/kitchen.png' className="h-24" />
+        </div>
+    )
+};
