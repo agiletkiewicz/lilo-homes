4a204e7869fee158f12a63e36542d3ab77f7c431
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-31 08:59:54 -0700

    links for bungalow work throughout

--- components/home/navbar.js
+++ components/home/navbar.js
@@ -4,11 +4,11 @@ import Image from 'next/image';
 export default function NavBar({ content }) {
     return (
         <nav className="flex justify-between sticky top-0 bg-white z-10"> 
-            <div className="pl-2 lg:justify-left"><Image src='/logo.png' width="100" height="72"/></div>
+            <a href="/"><div className="pl-2 lg:justify-left"><Image src='/logo.png' width="100" height="72"/></div></a>
             <div className="pr-3 pt-6 lg:hidden"><Image src='/menu.png' width="40" height="30"/></div>
             <ul className="hidden lg:flex lg:justify-right font-heading text-lg">
-                <li className="p-6">Home</li>
-                <li className="p-6">Bungalow</li>
+                <a href="/"><li className="p-6">Home</li></a>
+                <a href="/bungalow"><li className="p-6">Bungalow</li></a>
                 <li className="p-6">Location</li>
                 <li className="p-6">About</li>
             </ul>
