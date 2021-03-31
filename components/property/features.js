f5cf5ece74944aa681c135e9066c69964f6b12a3
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-30 21:40:20 -0700

    center features text on desktop

--- components/property/features.js
+++ components/property/features.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Features({ content }) {
     return (
-        <section className="bg-droplet-dark p-6"> 
+        <section className="bg-droplet-dark lg:px-40 lg:py-10 lg:text-center"> 
             <h1 className="font-heading text-white text-3xl lg:text-4xl pb-5">{content.heading}</h1>
             <p className="font-body text-lg text-white">{content.text}</p>
         </section>
