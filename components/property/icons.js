317552c96ca6ab05ff620a9b3b9a4b3d413de88e
agiletkiewicz <a.letkiewicz@gmail.com>
2021-03-30 21:53:39 -0700

    mkae icons mobile responsive

--- components/property/icons.js
+++ components/property/icons.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default function Icons() {
     return (
-        <div className="h-24 flex justify-center m-5">
-            <img src='/living.png' className="h-24" />
-            <img src='/washing.png' className="h-24" />
-            <img src='/smart.png' className="h-24" />
-            <img src='/kitchen.png' className="h-24" />
+        <div className="h-16 lg:h-24 flex justify-between lg:justify-center m-5">
+            <img src='/living.png' className="h-16 lg:h-24" />
+            <img src='/washing.png' className="h-16 lg:h-24" />
+            <img src='/smart.png' className="h-16 lg:h-24" />
+            <img src='/kitchen.png' className="h-16 lg:h-24" />
         </div>
     )
 };
