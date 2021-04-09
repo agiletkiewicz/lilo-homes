eb904eee2cdb4bb66c8b0616816465a2c30520d9
agiletkiewicz <a.letkiewicz@gmail.com>
2021-04-09 15:09:22 -0700

    add text to location page and style for desktop

--- components/location/map.js
+++ components/location/map.js
@@ -4,8 +4,11 @@ import React from 'react';
 export default function Map({ }) {
     return (
         <div className="flex justify-center">
-            <div className="p-5">
+            <div className="p-5 lg:hidden">
                 <iframe src="https://www.google.com/maps/d/embed?mid=1Q40fwBstIDQC1D_3iamolK7bmdFxscX5&hl=en" width="100%" height="240"></iframe>
+            </div>
+            <div className="p-5 hidden lg:block">
+                <iframe src="https://www.google.com/maps/d/embed?mid=1Q40fwBstIDQC1D_3iamolK7bmdFxscX5&hl=en" width="500" height="400"></iframe>
             </div>
         </div>
     )
--- pages/location.js
+++ pages/location.js
@@ -1,14 +1,15 @@
 import NavBar from '../components/home/navbar';
 import Footer from '../components/home/footer';
 import Map from '../components/location/map';
-
+import Heading from '../components/location/heading';
 
 
 export default function Home({ }) {
   return (
       <>
       <NavBar />
-      <Map />  
+      <Heading />
+      {/* <Map />   */}
       <Footer />
     </>
   )
