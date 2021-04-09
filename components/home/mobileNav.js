54730bc3a9bec08c3ba659bfd1051404102e4de1
agiletkiewicz <a.letkiewicz@gmail.com>
2021-04-09 15:15:37 -0700

    add shadow to menu

--- components/home/mobileNav.js
+++ components/home/mobileNav.js
@@ -2,7 +2,7 @@
 export default function MobileNav() {
 
     return (
-        <nav className="absolute top-20 right-2 bg-white p-5"> 
+        <nav className="absolute top-20 right-2 bg-white p-5 shadow-xl"> 
             <ul className="font-heading text-lg">
                 <a href="/house"><li className="p-0">Lilo House</li></a>
                 <a href="/bungalow"><li className="p-0">Lilo Bungalow</li></a>
--- pages/location.js
+++ pages/location.js
@@ -9,7 +9,6 @@ export default function Home({ }) {
       <>
       <NavBar />
       <Heading />
-      {/* <Map />   */}
       <Footer />
     </>
   )
