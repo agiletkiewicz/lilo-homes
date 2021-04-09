aaa203846b53a4e9d2e7cd2f43f77a3b13fda47f
agiletkiewicz <a.letkiewicz@gmail.com>
2021-04-09 14:55:14 -0700

    add locations page with map

---
+++ components/location/map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Map({ }) {
+    return (
+        <div className="flex justify-center">
+            <div className="p-5">
+                <iframe src="https://www.google.com/maps/d/embed?mid=1Q40fwBstIDQC1D_3iamolK7bmdFxscX5&hl=en" width="100%" height="240"></iframe>
+            </div>
+        </div>
+    )
+};
---
+++ pages/location.js
@@ -0,0 +1,16 @@
+import NavBar from '../components/home/navbar';
+import Footer from '../components/home/footer';
+import Map from '../components/location/map';
+
+
+
+export default function Home({ }) {
+  return (
+      <>
+      <NavBar />
+      <Map />  
+      <Footer />
+    </>
+  )
+}
+
