7b4a797ad5808cf33226b4a8f40a02427f73454a
agiletkiewicz <63075988+agiletkiewicz@users.noreply.github.com>
2021-05-14 10:21:09 -0700

    hotfix: images on house pages

--- components/property/hero.js
+++ components/property/hero.js
@@ -6,7 +6,7 @@ export default function Hero({ content, status, links }) {
         <>
             <h1 className="text-3xl lg:text-4xl text-main-blue p-2 font-heading lg:hidden">{content.heading}</h1>
             <header className="flex flex-wrap justify-center lg:justify-around pb-5">
-                <img src={content.image[0].url} className="object-cover h-100 lg:w-2/4 p-2" />
+                <img src={content.image[0].url} className="w-screen h-full lg:w-2/4 p-2" />
                 <div className="w-full lg:w-2/4">
                     <h1 className="text-3xl lg:text-5xl text-main-blue p-2 lg:p-6 font-heading hidden lg:block">{content.heading}</h1>
                     <h2 className="font-heading text-main-blue text-xl lg:text-xl p-2 lg:p-6">{status.text}</h2>
