dcf5de36493f55fad2fdbf6ba6be55e75bbcc171
agiletkiewicz <63075988+agiletkiewicz@users.noreply.github.com>
2021-05-14 09:45:26 -0700

    add copyright logo to footer

--- components/home/footer.js
+++ components/home/footer.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Footer({ }) {
     return (
         <div className="h-14 bg-white flex justify-between ml-5 mr-5 lg:ml-10 lg:mr-10 mt-5">
-            <p className="font-body text-xs lg:text-s">Copyright 2021 Stacey Smith</p>
-            <p className="font-body text-xs lg-text-s">Handcrafted by <a href="https://www.agiltanner.com" className="text-main-blue hover:underline">Agi</a></p>
+            <div className="font-body text-xs lg:text-s">
+                <img src="/copyright.png" className="h-3 inline" /> 
+                <p className="inline">  2021 Stacey Smith</p>
+            </div>
+            <div className="font-body text-xs lg-text-s">Handcrafted by <a href="https://www.agiltanner.com" className="text-main-blue hover:underline">Agi</a></div>
         </div>
     )
 };
