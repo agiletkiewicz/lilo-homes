33fbe1c82bd0621831c01863c52767dad8cbe743
Agi Tanner <agitanner@Agis-MacBook-Pro-2.local>
2023-12-20 22:01:26 -0800

    use img tag

--- components/home/navbar.js
+++ components/home/navbar.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Image from 'next/image';
 import MobileNav from './mobileNav';
 
 export default function NavBar({ content }) {
@@ -8,8 +7,8 @@ export default function NavBar({ content }) {
 
     return (
         <nav className="flex justify-between sticky top-0 bg-white z-10"> 
-            <a href="/"><div className="pl-2 lg:justify-left"><Image src='/logo.png' width="100" height="72"/></div></a>
-            <div className="pr-3 pt-6 lg:hidden" onClick={() => setOpen(!open)}><Image src='/menu.png' width="40" height="30"/></div>
+            <a href="/"><div className="pl-2 lg:justify-left"><img src='/logo.png' width="100" height="72"/></div></a>
+            <div className="pr-3 pt-6 lg:hidden" onClick={() => setOpen(!open)}><img src='/menu.png' width="40" height="30"/></div>
             {open ? <MobileNav /> : null}
             <ul className="hidden lg:flex lg:justify-right font-heading text-lg">
                 <a href="/house"><li className="p-6">Lilo House</li></a>
