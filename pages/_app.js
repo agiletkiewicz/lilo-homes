5058b3fa936e654e9c5532b6f7410272b75b3a66
Agi Tanner <agitanner@Agis-MacBook-Pro-2.local>
2023-12-31 12:17:27 -0800

    add meta properties to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import "tailwindcss/tailwind.css";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.lilohomes.com/" />
+          <meta property="og:title" content="Lilo Homes" />
+          <meta property="og:description" content="Find Lilo Homes properties and about information" />
+          <meta property="og:image" content="./public/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
