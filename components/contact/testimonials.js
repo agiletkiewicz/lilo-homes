b33e85e1066c3affd4d7f8a4bf09a58b5cc477b3
Agi Tanner <agitanner@Agis-MacBook-Pro-2.local>
2023-12-31 18:18:22 -0800

    styling upgrades to testimonials

--- components/contact/testimonials.js
+++ components/contact/testimonials.js
@@ -6,8 +6,8 @@ export default function Testimonials({ }) {
 
   return (
     <section className="lg:pl-20 lg:pr-20 pl-5 pr-5">
-      <div className="h-3/5">
-        <h1 className="text-3xl lg:text-5xl text-main-blue p-2 lg:p-6 font-heading lg:block">Testimonials</h1>
+      <h1 className="text-3xl lg:text-5xl text-main-blue p-2 lg:p-6 font-heading lg:block">Testimonials</h1>
+      <div className="italic whitespace-pre-wrap">
         <Carousel lastIdx={testimonials.length - 1} >
           <Carousel.ArrowLeft />
           <Carousel.Text texts={testimonials}/>
--- content/texts.js
+++ content/texts.js
@@ -26,13 +26,13 @@ export const home = {
   },
   comingSoon1: {
     heading: "Coming Soon",
-    text: "coming soon...",
+    text: "Coming soon, continue to detail page to learn more.",
     image: "/home-placeholder.png",
     key: "coming-soon-1"
   },
   comingSoon2: {
     heading: "Coming Soon",
-    text: "",
+    text: "Coming soon, continue to detail page to learn more.",
     image: "/home-placeholder.png",
     key: "coming-soon-2"
   }
@@ -100,11 +100,11 @@ export const comingSoon2 = {
 };
 
 export const testimonials = [
-  "“We rented the 3/1 Bungalow in 2021. The 1930 house is charming and in overall good condition and worked well for our situation at the time. We only had a couple of minor maintenance requests. Stacey quickly addressed the issues and worked with us to find the best solutions. The neighborhood is a convenient location to nearby shopping, rec center, and major roads. But also is quiet and we felt safe walking around. Stacy was respectful of our privacy and if she needed to come to the house she would notify us first. She was easy to interact with and a pleasant person overall.” - Past Tenant",
-  "“You should know that every time someone visits us, eyes light up and they comment on just how beautiful this house is. Your attention to detail and thoughtfulness to this place doesn't go unnoticed. We love it here.” - Past Tenant",
-  "“Stacey, I told my contractors what you saw when looking at my new townhome and they were so surprised and agreed those things you showed me should be fixed! They were so impressed I brought those things up that they asked me if I wanted to work for them... So, you have a job here in Houston if you need one!” - Past Client",
-  "“Thank you for telling me about these things with my staff; you’d make a great site manager. I’d hire you in a heartbeat if you ever want to get into construction.” - Past Contractor",
-  "Per a walkthrough showing my drawings for a foundation renovation. \"Who did this? This is better than most engineers I've worked with\" - Masonry and Excavating Company",
+  "“We rented the 3/1 Bungalow in 2021. The 1930 house is charming and in overall good condition and worked well for our situation at the time. We only had a couple of minor maintenance requests. Stacey quickly addressed the issues and worked with us to find the best solutions. The neighborhood is a convenient location to nearby shopping, rec center, and major roads. But also is quiet and we felt safe walking around. Stacy was respectful of our privacy and if she needed to come to the house she would notify us first. She was easy to interact with and a pleasant person overall.”\n\n- Past Tenant",
+  "“You should know that every time someone visits us, eyes light up and they comment on just how beautiful this house is. Your attention to detail and thoughtfulness to this place doesn't go unnoticed. We love it here.”\n\n- Past Tenant",
+  "“Stacey, I told my contractors what you saw when looking at my new townhome and they were so surprised and agreed those things you showed me should be fixed! They were so impressed I brought those things up that they asked me if I wanted to work for them... So, you have a job here in Houston if you need one!”\n\n- Past Client",
+  "“Thank you for telling me about these things with my staff; you’d make a great site manager. I’d hire you in a heartbeat if you ever want to get into construction.”\n\n- Past Contractor",
+  "Per a walkthrough showing my drawings for a foundation renovation. \"Who did this? This is better than most engineers I've worked with\"\n\n- Masonry and Excavating Company",
 ];
 
 export const faqs = [
