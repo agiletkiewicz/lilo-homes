c796f36dd7fa4790bb670721d152cf65f76fd943
Staceymae <53414565+Staceymae@users.noreply.github.com>
2026-04-05 20:35:02 -0400

    Update texts.js

--- content/texts.js
+++ content/texts.js
@@ -1,26 +1,26 @@
 export const home = {
   hero: {
-    heading: "Live in Utica, NY",
+    heading: "Heritage Homes, Modern Soul.",
     image: "/home-hero.jpeg"
   },
   home: {
     heading: "More than \"just\" rentals",
-    text: "Beautiful eclectic 1930's homes for rent. Think: history meets tech meets cozy vibes. Lilo homes are meticulously cared for with attention to detail of utmost importance for embracing originality."
+    text: "Enjoy a sense of "home" that prevents the burnout common in extended hotel stays. Lilo homes are meticulously cared for with attention to detail of utmost importance."
   },
   about: {
-    heading: "The creative behind the mission",
-    text: "Meet Stacey Smith, the heart and visionary behind Lilo Homes. With a passion for preserving history and creating inviting communities, Stacey is on a mission to turn houses into loving homes. Her caring nature and commitment shine through in every Lilo Home, inviting you to be a part of their timeless stories. \"Exploring and immersing myself in new experiences, different cultures, and witnessing the [hi]stories of places that stand the test of time is soul-filling for me. While travel is an integral part of my life, a stable home and community is also of high value to me.\"",
+    heading: "The Visionary Behind the Mission",
+    text: "Meet Stacey Smith, the heart of Lilo Homes. A traveler at spirit with a deep reverence for stability and community, Stacey turns historic houses into loving homes.\I believe in soul-filling experiences—immersing myself in different cultures while maintaining a high value for a stable, beautifully cared-for home. Lilo is where history meets that modern need for peace."",
     image: "/home-about.png"
   },
   bungalow: {
     heading: "Lilo Bungalow",
-    text: "This South Utica bungalow has 3 bedrooms and 1 bathroom with beautiful original hardwoods, a private fenced-in yard, an updated bathroom, and a modern aesthetic combined with 1930's architectural features.",
+    text: "This South Utica gem offers a cozy yet modern aesthetic. Enjoy refinished hardwoods, bright kitchen, updated bath, and a private fenced-in yard. Fully integrated with Nest smart features, it’s a peaceful retreat on the bus line, just minutes from the best of the city.",
     image: "/home-bungalow.jpeg",
     key: "bungalow"
   },
   house: {
     heading: "Lilo House",
-    text: "This beautiful + bright 1930s house transformed into a \"smarter\" home in 2018. This special home was cared for meticulously by its original builders' family (two generations), has been updated with a beautiful kitchen re-designed with great intention.",
+    text: "While cared for meticulously by the original builder and family for two generations, this bright home has been updated with a beautiful kitchen with great intention and designed with the modern professional in mind.",
     image: "/home-house.png",
     key: "house"
   },
@@ -31,26 +31,26 @@ export const home = {
     key: "residence"
   },
   flats: {
-    heading: "Emerson Flats",
-    text: "Coming soon, continue to detail page to learn more.",
-    image: "/flats.png",
-    key: "flats"
+    heading: "Baker Abode",
+    text: "The Baker Abode has the largest tree on the block with a cozy back porch to relax and unwind on. With it's travel-inspired decor, a modern kitchen, reverse osmosis water filtration system, and charming original features- you're bound to feel at home and well cared for.",
+    image: "/abode.png",
+    key: "Abode"
   }
 };
 
 export const bungalow = {
   hero: {
     heading: "Lilo Bungalow",
-    text: "This South Utica bungalow is up for hosting a roommate. The home has beautiful refinished original hardwoods, a fenced-in yard, an updated bath, and a modern aesthetic combined with 1930's architectural features. Big-ticket items have mostly been replaced in the last few years from hot water heater, forced air furnace, seamless gutters, and roof.",
+    text: "This home has been a perfect fit for corporate housing in Utica, NY. The home has beautiful refinished original hardwoods, a fenced-in yard, an updated bath, and a modern aesthetic combined with 1930's architectural features. Big-ticket items have mostly been replaced in the last few years from hot water heater, forced air furnace, seamless gutters, and roof.",
     image: "/bungalow-hero.jpeg"
   },
   features: {
     heading: "Features",
-    text: "Has living room, dining room, kitchen, 1 bath, 2 rooms available (1 room currently used as an office), washer and dryer in basement, single stall garage, fenced-in backyard, on the bus line, Smart home features (Nest door lock, thermostat, and smoke detectors), utilities included, furnished & fantastic neighborhood.",
+    text: "Has living room, dining room, kitchen, 1 bath, 2 bedrooms separate areas of the home, office room with printer, washer and dryer in basement, fenced-in backyard, on the bus line, Smart home features and security cameras, utilities included, furnished & fantastic neighborhood.",
   },
   images: ["/bungalow1.jpeg", "/bungalow2.jpeg", "/bungalow3.jpeg", "/bungalow4.jpeg", "/bungalow5.jpeg", "/bungalow6.jpeg", "/bungalow7.jpeg", "/bungalow8.jpeg"],
   status: {
-    text: "Pending Availabilty: May 1, 2024",
+    text: "Inquire for availability",
   },
   icons: ['/living.png', '/washing.png', '/smart.png', '/kitchen.png'],
   links: [{url: "https://rental.turbotenant.com/p/storrs-ave-utica-ny/296545", content: "Rent"}, {url: "https://www.airbnb.com/rooms/46414569?s=67&unique_share_id=373fdc1f-4d39-4215-a425-1fc7b1220946&source_impression_id=p3_1613800287_TYMeMLTQ2RafXIxU", content: "Airbnb"}]
@@ -59,15 +59,15 @@ export const bungalow = {
 export const house = {
   hero: {
     heading: "Lilo House",
-    text: "Beautiful + bright 1930's house transformed into a \"smarter\" home. This special home has been updated, cared for meticulously and it's craving tidy humans to occupy it. Have your privacy in a peaceful neighborhood with more space; compare to downtown lofts.",
+    text: "Beautiful + bright 1930's house transformed into a \"smarter\" home with security cameras and completely re-wired for modern professionals. This special home has been updated, cared for meticulously and deserves occupants who appreciate its grand presence. Have your privacy in a peaceful neighborhood with more space; compare to downtown lofts.",
     image: "/house-hero.png"
   },
   features: {
     heading: "Features",
-    text: "High-quality kitchen, gas stove, quartz countertops, Nest lock, Nest temp control, central A/C, high ceilings, 1930's features (built-in cabinets, bookshelves, original refinished hardwood floors), storage shed, basement & attic space, \"concierge\" doc & Google Map w/ local recommendations (from babysitters to picnic spots).",
+    text: "High-quality kitchen, gas stove, quartz countertops, Nest lock, Nest temp control, central A/C, high ceilings, 1930's features (built-in cabinets, bookshelves, original refinished hardwood floors), basement access, \"concierge\" binder & Google Map w/ local recommendations (from babysitters to picnic spots).",
   },
   status: {
-    text: "Pending availability: May 1, 2024",
+    text: "Inquire for availability",
   },
   images: ["/house1.jpeg", "/house2.jpeg", "/house3.jpeg", "/house4.jpeg", "/house5.jpeg", "/house6.jpeg", "/house7.jpeg", "/house8.jpeg", "/house9.jpeg", "/house10.jpeg", "/house11.jpeg", "/house12.jpeg", "/house13.jpeg"],
   icons: ['/cabinet.png', '/storage.png', '/smart.png', '/kitchen.png'],
@@ -77,11 +77,11 @@ export const house = {
 export const residence = {
   hero: {
     heading: "Briar Residence",
-    text: "Coming soon.\n\nThis single-family home will be restored into a charming and unique floor plan including a vaulted ceiling upon entry, a loft space over the living room, and additional living space on the second floor. It’s in a quiet peaceful neighborhood with low traffic right around the corner from the grocery store and has a sizeable backyard.\n\nAttention Local Organizations: We welcome feedback on the development of this project to accommodate the needs of your organization for housing employees and their families. Get in touch to open a conversation about working together.",
+    text: "Currently under restoration, Briar will feature a striking vaulted ceiling entry and a unique lofted living space. We are actively seeking feedback from local organizations to tailor this development to the specific needs of their relocating employees.",
     image: "/residence.png"
   },
   status: {
-    text: "Pending Availability: March 1, 2025",
+    text: "Coming Soon!",
   },
   links: [{content: "Join Waitlist", url: "https://docs.google.com/forms/d/e/1FAIpQLSe2AV9ReeaHBcaw2tiYY8Qs438ku7fzK3yzUvWPBftc7QC9WA/viewform?usp=sf_link"}],
   images: ["/coming-soon-1.png"],
@@ -89,12 +89,12 @@ export const residence = {
 
 export const flats = {
   hero: {
-    heading: "Emerson Flats",
-    text: "Coming soon.\n\nThis 1920s multi-family home is being rehabilitated into comfy residences in South Utica right next to Cafe Domenico’s, Slice pizzeria, and the Uptown theatre. It will have a mix of long-term, mid-term, and short-term rentals furnished with up-cycled furniture and bold styles.\n\nAttention Local Organizations: We welcome feedback on the development of this project to accommodate the needs of your organization for housing employees and their families. Get in touch to open a conversation about working together.",
+    heading: "Baker Abode",
+    text: "Home to the largest tree on the block, the Baker Abode is designed for those who need to unplug after a high-demand day. This travel-inspired space features a cozy back porch, a reverse osmosis water filtration system, and a modern kitchen. It’s where historic charm meets a high standard of self-care.",
     image: "/flats.png"
   },
   status: {
-    text: "Pending Availability: June 1, 2024",
+    text: "Inquire for availability",
   },
   links: [{content: "Join Waitlist", url: "https://docs.google.com/forms/d/e/1FAIpQLSe2AV9ReeaHBcaw2tiYY8Qs438ku7fzK3yzUvWPBftc7QC9WA/viewform?usp=sf_link"}],
   images: ["/coming-soon-2-1.png", "/coming-soon-2-2.png"],
@@ -109,14 +109,13 @@ export const testimonials = [
 ];
 
 export const faqs = [
-  "Interested in renting, but nothing showing as available? There’s no harm in sending me a message as I know some other great folks who may be able to support your search for housing and accommodations.",
-  "Interested in mobilizing more Lilo Homes as a private investor? If you see the vision, I am receptive. While profit is important for sustaining high-quality offerings and quality of life, the care of the homes and our guests are just as important to me. Let’s have a conversation if this aligns.",
-  "Interested in real estate, property management, renovating properties, etc.? I am an advocate for the trade economy. I love idea-sharing and I love continued learning. Pitch an idea to trade skills or contribute to learning the ropes with me!",
+  "Interested in renting, but nothing showing as available? There’s no harm in sending us a message as I know some other great folks who may be able to support your search for housing and accommodations.",
+  "Interested in mobilizing more Lilo Homes as a private investor? If you see the vision, let's talk.",
+  "Interested in real estate, property management, renovating properties, etc.? I am an advocate for the trade economy. We love idea-sharing and we love continued learning. Pitch an idea to trade skills or contribute to learning the ropes with us!",
 ];
 
-export const location = "All of the Lilo Homes are located in the South Utica neighborhood. The Storrs Ave Homes are 500ft from the Parkway Center, which is equipped with a playground, tennis courts, basketball courts, and a ski chalet. All of the homes are also within 800 meters of grocery stores, gas stations, restaurants, and cafes including Cafe Domenico’s, Slice Pizzeria, The Green Onion Pub, and Bagel Grove. The Homes are within 3 miles of Utica College, Mohawk Valley Community College, 2 co-working spaces, and St Elizabeth’s College of Nursing. A few exciting locations in the neighborhood also include the brand-new Utica Children’s Museum on the Memorial Parkway, The Uptown Theatre for Creative Arts, and the Midtown Utica Community Center.";
+export const location = "All of the Lilo Homes are located in the South Utica neighborhood. The Storrs Ave Homes are 500ft from the Parkway Center, which is equipped with a playground, tennis courts, basketball courts, pickleball, ice skating, and a ski chalet. All of the homes are also within 800 meters of grocery stores, gas stations, restaurants, and cafes including Emerson Cafe, Slice Pizzeria, The Green Onion Pub, and Bagel Grove. The Homes are within 3 miles of Utica College, Wolfspeed Mohawk Valley Community College, co-working spaces, and St Elizabeth’s College of Nursing. A few exciting locations in the neighborhood also include the Utica Zoo, brand-new Utica Children’s Museum on the Memorial Parkway, The Uptown Theatre for Creative Arts, and the Midtown Utica Community Center.";
 
 export const offers = [
-  { content: "For Landlords: Accounting with REI Hub - Use my name to Claim $50\n(click to continue)", url: "https://www.reihub.net/", image: "/REIHub.png" },
   { content: "For Landlords: Manage with TurboTenant - Give $25. Get $25\n(click to continue)", url: "https://turbotenant.com/r/T3duZXI6MjgzNDAz", image: "/TurboTenant.png" },
 ]
